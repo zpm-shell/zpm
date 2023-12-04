27c61e11c7ec7c325dc54a877ac6fcbfd44da49a
wuchuheng <root@wuchuheng.com>
2023-12-04 20:26:06 +0800

    feat: add a quckjs

---
+++ src/js-scripts/libs/quckjs_args_parser/src/index.ts
@@ -0,0 +1,189 @@
+type CommandType = {
+  name: string;
+  description: string;
+  args: {
+    name: string;
+    type: "string" | "boolean";
+    description: string;
+  }[];
+  options: {
+    name: string;
+    alias: string;
+    required: boolean;
+    type: "string" | "boolean";
+    description: string;
+  }[];
+};
+
+type OutputType = {
+  regularArgs: string[];
+  options: Record<string, boolean | string>;
+};
+
+enum ErrorType {
+  PRINT_HELP = "print help",
+  // the count of the arguments is not correct.
+  INCORRECT_ARGUMENT_COUNT = "incorrect argument count",
+
+  // the count of the options is not correct.
+  INCORRECT_OPTION_COUNT = "incorrect option count",
+}
+
+function printHelp(command: CommandType) {
+  let argsStr = "";
+  command.args.forEach((arg) => {
+    argsStr += ` <${arg.name}>`;
+  });
+  if (command.options.length > 0) {
+    argsStr += " [options]";
+  }
+  console.log(`Usage: ${command.name} ${argsStr}`);
+  console.log("Options:");
+  command.options.forEach((option) => {
+    console.log(`  --${option.name}, -${option.alias}\t${option.description}`);
+  });
+  console.log("");
+  console.log(`${command.description}`);
+}
+
+async function parseArguments(
+  command: CommandType,
+  args: string[]
+): Promise<OutputType> {
+  const options: Record<string, boolean | string> = {};
+  const regularArgs = [];
+  let currentOption = "";
+
+  // if the args include the --help option, print the help message and exit.
+  if (args.includes("--help")) {
+    printHelp(command);
+    throw new Error(ErrorType.PRINT_HELP);
+  }
+
+  // Mapping of the alias name to the long name in the command options list
+  const aliasMap: Record<string, string> = {};
+  for (
+    let optionIndex = 0;
+    optionIndex < command.options.length;
+    optionIndex++
+  ) {
+    const optionName = command.options[optionIndex].name;
+    const alias = command.options[optionIndex].alias;
+    aliasMap[alias] = optionName;
+  }
+
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+
+    if (arg.startsWith("--")) {
+      currentOption = arg.substring(2);
+      options[currentOption] = true; // Set a default value
+    } else if (arg.startsWith("-")) {
+      const aliasName = arg.substring(1);
+      currentOption = aliasMap[aliasName];
+      options[currentOption] = true; // Set a default value
+    } else {
+      if (currentOption.length !== 0) {
+        // Assign the value to the last option
+        options[currentOption] = arg;
+        currentOption = "";
+      } else {
+        // Regular argument
+        regularArgs.push(arg);
+      }
+    }
+  }
+
+  // check the total number of arguments
+  if (regularArgs.length !== command.args.length) {
+    throw new Error(ErrorType.INCORRECT_ARGUMENT_COUNT);
+  }
+
+  // check the required options was existed or not
+  command.options
+    .filter((e) => e.required)
+    .forEach((el) => {
+      // eslint-disable-next-line no-prototype-builtins
+      if (!options.hasOwnProperty(el.name)) {
+        throw new Error(ErrorType.INCORRECT_OPTION_COUNT);
+      }
+    });
+
+  return { regularArgs, options };
+}
+function parseArgs(
+  commandConfigs: CommandType,
+  args: string[]
+): Promise<OutputType> {
+  return new Promise<OutputType>((resolve, reject) => {
+    parseArguments(commandConfigs, args)
+      .then((result) => resolve(result))
+      .catch((err) => {
+        if (err.message != ErrorType.PRINT_HELP) {
+          console.log(
+            `${commandConfigs.name}: try '${commandConfigs.name} --help' for more information`
+          );
+        }
+        return reject(err);
+      });
+  });
+}
+
+// const args = [
+//   "command1",
+//   "arg1",
+//   "arg2",
+//   "--option1",
+//   "option1 value",
+//   "--option2",
+//   "option2 value",
+//   "-a",
+//   "option3 value",
+//   "-c",
+//   "option4 value",
+// ];
+// const commandConfigs: CommandType = {
+//   name: "qjs.decodeBase64",
+//   description: "qjs.decodeBase64 <arg1: base64 encode string> [options]",
+//   args: [
+//     {
+//       name: "arg1",
+//       type: "string",
+//       description: "Argument 1 description",
+//     },
+//     {
+//       name: "arg2",
+//       type: "string",
+//       description: "Argument 2 description",
+//     },
+//   ],
+//   options: [
+//     {
+//       type: "string",
+//       name: "option1",
+//       alias: "o1",
+//       description: "option 1",
+//     },
+//     {
+//       type: "string",
+//       name: "option2",
+//       alias: "o2",
+//       description: "option 1",
+//     },
+//     {
+//       type: "string",
+//       name: "option3",
+//       alias: "a",
+//       description: "option 1",
+//     },
+//     {
+//       type: "string",
+//       name: "option4",
+//       alias: "c",
+//       description: "option 1",
+//     },
+//   ],
+// };
+
+export { ErrorType };
+export default parseArgs;
