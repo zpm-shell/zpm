dca56806d54cb6aa42361b8900534822cad7d79c
wuchuheng <root@wuchuheng.com>
2023-12-06 01:23:54 +0800

    test(unit): add an unit test to test an import function with the absolute path

--- test/unit/autoload.test/autoload.test.zsh
+++ test/unit/autoload.test/autoload.test.zsh
@@ -3,9 +3,22 @@
 . "${ZMOD_DIR}/src/autoload.zsh"
 
 import --from ./m1.zsh --as m1
-import --from @/test/unit/autoload.test/m2.zsh --as m2
 
 function test_import() {
   local actualVal=$( ${ZMOD_DIR}/test/unit/autoload.test/m1.zsh:3:m1 )
   expect_equal --actual "${actualVal}" --expected "m1"
+}
+
+function test_import_at_file_path() {
+  import --from @/test/unit/autoload.test/m2.zsh --as m2
+  expect_equal --actual "$?" --expected "0"
+  local actualVal=$( ${ZMOD_DIR}/test/unit/autoload.test/m2.zsh:3:m2 )
+  expect_equal --actual "${actualVal}" --expected "m2 func"
+}
+
+function test_import_with_absolute_path() {
+  import --from "${ZMOD_DIR}/test/unit/autoload.test/m3.zsh" --as m3
+  expect_equal --actual "$?" --expected "0"
+  local actualVal=$( ${ZMOD_DIR}/test/unit/autoload.test/m3.zsh:3:m3 )
+  expect_equal --actual "${actualVal}" --expected "m3 func"
 }
