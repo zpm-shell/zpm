2e42028a39d0cac1e2b6b36aab980e27861e18ba
wuchuheng <root@wuchuheng.com>
2023-12-06 01:18:17 +0800

    test(unit): add a unit test that to use the import function with the @/.. file path

--- test/unit/autoload.test/autoload.test.zsh
+++ test/unit/autoload.test/autoload.test.zsh
@@ -3,6 +3,7 @@
 . "${ZMOD_DIR}/src/autoload.zsh"
 
 import --from ./m1.zsh --as m1
+import --from @/test/unit/autoload.test/m2.zsh --as m2
 
 function test_import() {
   local actualVal=$( ${ZMOD_DIR}/test/unit/autoload.test/m1.zsh:3:m1 )
