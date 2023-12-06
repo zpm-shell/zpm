1823f8d451778c04027ca7d643581c05d3b985fe
wuchuheng <root@wuchuheng.com>
2023-12-07 00:28:05 +0800

    feat: add the feature to create a zmod.json5

--- src/mod-boot.zsh
+++ src/mod-boot.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
+TRUE=0
+FALSE=1
+
 local ZMOD_VERSION="0.0.1"
 
 function print_zmod_docs() {
@@ -8,17 +11,52 @@ zmod <command> [ script.zsh ]
 
 Usage:
 
-zmod install        install all the dependencies in your project
-zmod install <foo>  add the <foo> dependency to your project
-zmod test           run this project's tests
-zmod run <foo>      run the script named <foo>
-zmod init           create a zmod.json5 file
-zmod --help,-h      print the help documentation
-zmod -v,--version   print the help documentation
-zmod [script.zsh]   execute the script
+zmod install                                install all the dependencies in your project
+zmod install <foo>                          add the <foo> dependency to your project
+zmod test                                   run this project's tests
+zmod run <foo>                              run the script named <foo>
+zmod init <repository/username/module-name> create a zmod.json5 file
+zmod --help,-h                              print the help documentation
+zmod -v,--version                           print the help documentation
+zmod [script.zsh]                           execute the script
 
 zmod@${ZMOD_VERSION} ${ZMOD_DIR}/bin/zmod
 EOF
+}
+
+##
+# create a zmod.json5
+# @params $1 {string} the package name
+# @return <boolean>
+##
+function create_zmod_json5() {
+
+  local zmodJson5="zmod.json5"
+  if [[ -f ${zmodJson5} ]]; then
+    echo "${zmodJson5} already exitst"
+    exit ${FALSE};
+  fi
+  local packageName="$1"
+  if [[ -z ${packageName} ]]; then
+      local currentDir=$(pwd)
+      local packageName=${currentDir##*/}
+  fi
+  cat > ${zmodJson5} << EOF
+{
+  "name": "${packageName}",
+  "version": "1.0.0",
+  "description": "",
+  "main": "lib/main.zsh",
+  "scripts": {
+    "start": "echo \"hello, ${packageName}\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "MIT"
+}
+EOF
+
+cat ${zmodJson5};
 }
 
 if [[ $# -eq 0 ]]; then
@@ -36,6 +74,11 @@ for (( i = 1; i <= ${#args[@]}; i++ )); do
       ;;
     --version|-v)
       echo "${ZMOD_VERSION}"
+      exit 0
+      ;;
+    init)
+      shift
+      create_zmod_json5
       exit 0
       ;;
     *)
