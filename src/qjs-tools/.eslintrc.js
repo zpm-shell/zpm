19dbf8cf96db370cdb7c6d7cb0140a808b5981da
wuchuheng <root@wuchuheng.com>
2023-12-13 22:42:30 +0800

    chore: update the config for eslint and typescript

--- src/qjs-tools/.eslintrc.js
+++ src/qjs-tools/.eslintrc.js
@@ -29,5 +29,6 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/explicit-function-return-type": "error", // or "error" if you want it to fail the linting process
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
 };
