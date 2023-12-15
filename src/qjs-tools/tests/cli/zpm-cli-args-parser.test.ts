49dbb99d596377a52d00f02a810dff09460b3629
wuchuheng <root@wuchuheng.com>
2023-12-15 22:18:51 +0800

    test(qjs-tools): add test for bin/zpm-cli-args-parser

--- src/qjs-tools/tests/cli/zpm-cli-args-parser.test.ts
+++ src/qjs-tools/tests/cli/zpm-cli-args-parser.test.ts
@@ -10,4 +10,18 @@ describe("zpm-cli-args-parser tests", () => {
       "'}";
     expect(json5).toBe(expectedVal);
   });
+
+  it("should print the documents in JSON5", () => {
+    let json5: string = execSync("./bin/zpm-cli-args-parser --help").toString();
+    json5 = json5.trim();
+    const expectedVal =
+      "{success:true,printTxt:" +
+      "'Usage: zpm [command] [options]\\n\\nVersion: 0.0.1\\nzpm is a package manager for zsh\\nCommands:\\n  zpm init\\t\\tcreate a zpm.json5 file\\n\\nGlobal options:\\n  -h, --help\\t\\tShow this help message and exit\\n  -v, --version\\t\\tShow version information and exit\\n" +
+      "'}";
+    expect(json5).toBe(expectedVal);
+
+    json5 = execSync("./bin/zpm-cli-args-parser -h").toString();
+    json5 = json5.trim();
+    expect(json5).toBe(expectedVal);
+  });
 });
