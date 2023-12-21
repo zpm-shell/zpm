cbb77c50b6961a55361f26465aedd00c25287ea0
wuchuheng <root@wuchuheng.com>
2023-12-21 18:05:58 +0800

    feat: Implement get function in ref_list for item retrieval by index
    - Added  function to .
    - The function allows retrieval of list elements by index.
    - Includes input validation for reference name, index value, and type checks.
    - Ensures index is within the valid range of the list and handles errors appropriately.

--- src/utils/ref_list.zsh
+++ src/utils/ref_list.zsh
@@ -121,3 +121,56 @@ function values() {
         echo \${${inputRef}[@]}
     "
 }
+
+##
+# get get the value of the list reference variable by index.
+# @--param --ref|-r: the reference variable name.
+# @--param --index|-i: the index of the value.
+# @echo <string>
+function get() {
+    local inputRef=''
+    local inputIndex=''
+    local args=("$@")
+    local i;
+    for (( i = 1; i <= ${#args}; i++ )); do
+        local arg="${args[$i]}"
+        if [[ "$arg" == '--ref' || "$arg" == '-r' ]]; then
+            inputRef="${args[$i+1]}"
+        elif [[ "$arg" == '--index' || "$arg" == '-i' ]]; then
+            inputIndex="${args[$i+1]}"
+        fi
+    done
+    # check the input value
+    if [[ -z ${inputRef} ]]; then
+        throw --error-message "The param --ref|-r required"
+    elif [[ -z ${inputIndex} ]]; then
+        throw --error-message "The param --index|-i required"
+    fi
+    # check the input index must be a number and greater than 0
+    if [[ ! ${inputIndex} =~ ^[0-9]+$ ]]; then
+        throw --error-message "The param --index|-i must be a number"
+        return ${FALSE}
+    elif [[ ${inputIndex} -lt 1 ]]; then
+        throw --error-message "The param --index|-i must be greater than 0"
+        return ${FALSE}
+    fi
+
+    # check the reference variable type
+    local actualType=$(typeset -p ${inputRef} 2>/dev/null)
+    if [[ "$actualType" != 'typeset -g -a '* ]]; then
+        throw --error-message "The variable ${inputRef} is not a list reference variable"
+        return ${FALSE}
+    fi
+
+    # check the index exists in the list
+    local len=$(eval "echo \${#${inputRef}[@]}")
+    if [[ ${len} -lt ${inputIndex} ]]; then
+        throw --error-message "The index ${inputIndex} is out of range"
+        return ${FALSE}
+    fi
+
+    # echo the value
+    eval "
+        echo \${${inputRef}[$inputIndex]}
+    "
+}
