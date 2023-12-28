300ea2a6850c4a9cff374b4cb5d4073846729754
wuchuheng <root@wuchuheng.com>
2023-12-28 18:12:22 +0800

    test(unit): Update unit tests for log functionality with ZPM_LOG_LEVEL

--- test/unit/utils/log.test.zsh
+++ test/unit/utils/log.test.zsh
@@ -1,10 +1,13 @@
 import ../../../src/utils/log.zsh --as log
 
 function test_debug_log() {
+    local old_zpm_log_level="${ZPM_LOG_LEVEL}";
+    ZPM_LOG_LEVEL="DEBUG"
     local actual="$( call log.debug hello)"
+    ZPM_LOG_LEVEL="${old_zpm_log_level}"
     local currentFilePath="test/unit/utils/log.test.zsh"
     currentFilePath=${currentFilePath}
-    local expectVal="${currentFilePath}:4 [\e[1;93mDEBUG\e[0m] hello"
+    local expectVal="${currentFilePath}:6 [\e[1;93mDEBUG\e[0m] hello"
     expectVal=$(echo ${expectVal})
     expect_equal --expected "${expectVal}" --actual "${actual}"
 }
@@ -13,7 +16,7 @@ function test_info_log() {
     local actual="$( call log.info hello)"
     local currentFilePath="test/unit/utils/log.test.zsh"
     currentFilePath=${currentFilePath}
-    local expectVal="${currentFilePath}:13 [\e[1mINFO\e[0m] hello"
+    local expectVal="${currentFilePath}:16 [\e[1mINFO\e[0m] hello"
     expectVal=$(echo ${expectVal})
     expect_equal --expected "${expectVal}" --actual "${actual}"
 }
