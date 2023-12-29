5bb6a9ad7c474be1465e9ac26f49e4998d7bece3
wuchuheng <root@wuchuheng.com>
2023-12-29 22:54:12 +0800

    test(json5-query): Update unit tests to include '-t get' option
    - Modified the unit tests for the `json5-query` CLI tool to include the `-t get` option in the command execution.
    - Ensured the tests accurately reflect the updated usage of the `json5-query` tool, which now requires the `-t get` option for direct value retrieval.

--- src/qjs-tools/tests/cli/json5-query.test.ts
+++ src/qjs-tools/tests/cli/json5-query.test.ts
@@ -4,13 +4,13 @@ import { execSync } from "child_process";
 describe("json5-query cli test", () => {
   it("should execute the json5-query cli correctly", () => {
     const result = execSync(
-      `./bin/json5-query -j '{hello:"world"}' -q hello`
+      `./bin/json5-query -j '{hello:"world"}' -q hello -t get`
     ).toString();
     expect(result).toBe("world\n");
   });
   it("should return the false value", () => {
     const result = execSync(
-      `./bin/json5-query -j '{hello: false}' -q hello`
+      `./bin/json5-query -j '{hello: false}' -q hello -t get`
     ).toString();
     expect(result).toBe("false\n");
   });
