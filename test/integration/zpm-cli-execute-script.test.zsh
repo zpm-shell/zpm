851ff766a96a9ef36a9fbc953a7e0088bafe65fc
wuchuheng <root@wuchuheng.com>
2023-12-30 18:22:33 +0800

    test(zpm-cli): Reactivate and update module loading integration test
    - Re-enabled the `test_example_module_loading` function in `zpm-cli-execute-script.test.zsh`.
    - Updated the test to use the `run` command for executing the `lib/main.zsh` script within the ZPM workspace.
    - The test asserts the successful execution of the script and validates the expected output.

--- test/integration/zpm-cli-execute-script.test.zsh
+++ test/integration/zpm-cli-execute-script.test.zsh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 
-# function test_example_module_loading() {
-#     local actualVal;
-#     actualVal=$(
-#         cd ${ZPM_DIR}/example/module-loading;
-#         ZPM_WORKSPACE="${ZPM_DIR}/example/module-loading" ${ZPM_DIR}/bin/zpm lib/main.zsh 2>&1 
-#     )
-#     expect_equal --actual "$?" --expected "0"
-#     local expectedVal="hello"
-#     expect_equal --actual "${actualVal}" --expected "${expectedVal}"
-# }
+function test_example_module_loading() {
+    local actualVal;
+    actualVal=$(
+        cd ${ZPM_DIR}/example/module-loading;
+        ZPM_WORKSPACE="${ZPM_DIR}/example/module-loading" ${ZPM_DIR}/bin/zpm run lib/main.zsh
+    )
+    expect_equal --actual "$?" --expected "0"
+    local expectedVal="hello"
+    expect_equal --actual "${actualVal}" --expected "${expectedVal}"
+}
