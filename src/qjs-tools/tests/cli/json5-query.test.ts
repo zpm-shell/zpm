aa04ef542372b7d089ce9aa316f79867a4fe4b57
wuchuheng <root@wuchuheng.com>
2024-01-13 20:35:19 +0800

    test: Correct JSON string format in `jq` CLI tests
    - Fixed the JSON strings in the test cases to use double quotes for object keys.
    - This ensures that the test strings are valid JSON, aligning with JSON standards.
    
    This commit corrects the test cases to prevent syntax errors and ensure that the tests reflect real-world usage of the `jq` CLI tool with properly formatted JSON input.

--- src/qjs-tools/tests/cli/json5-query.test.ts
+++ src/qjs-tools/tests/cli/json5-query.test.ts
@@ -4,13 +4,13 @@ import { execSync } from "child_process";
 describe("jq cli test", () => {
   it("should execute the jq cli correctly", () => {
     const result = execSync(
-      `./bin/jq -j '{hello:"world"}' -q hello -t get`
+      `./bin/jq -j '{"hello":"world"}' -q hello -t get`
     ).toString();
     expect(result).toBe("world\n");
   });
   it("should return the false value", () => {
     const result = execSync(
-      `./bin/jq -j '{hello: false}' -q hello -t get`
+      `./bin/jq -j '{"hello": false}' -q hello -t get`
     ).toString();
     expect(result).toBe("false\n");
   });
